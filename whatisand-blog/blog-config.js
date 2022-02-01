559245ef90ca03541e2b15536e1f4fbc3a27e773
Kim Dongwoo <andandend2847@gmail.com>
2022-02-01 18:01:26 +0900

    init

--- whatisand-blog/blog-config.js
+++ whatisand-blog/blog-config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  title: "whatisand",
+  title: "Kim Dongwoo",
   description: "세상과 다른 방법으로 살고싶습니다.",
-  author: "Dongwoo Kim",
+  author: "Kim Dongwoo",
   siteUrl: "https://whatisand.github.io/blog/",
   links: {
     github: "https://github.com/whatisand",
     linkedIn: "https://linkedin.com",
-    facebook: "https://www.facebook.com/kimdongwoo",
-    instagram: "https://www.instagram.com/dwkim_and",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com",
     email: "mailto:andandend2847@gmail.com",
   },
   utterances: {
