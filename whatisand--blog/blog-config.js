0bb9ddac1f6b165ff747821cbcc38f6dc43c90c0
Kim Dongwoo <andandend2847@gmail.com>
2022-02-01 17:51:35 +0900

    Init blog

---
+++ whatisand--blog/blog-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: "whatisand",
+  description: "세상과 다른 방법으로 살고싶습니다.",
+  author: "Dongwoo Kim",
+  siteUrl: "https://whatisand.github.io/blog/",
+  links: {
+    github: "https://github.com/whatisand",
+    linkedIn: "https://linkedin.com",
+    facebook: "https://www.facebook.com/kimdongwoo",
+    instagram: "https://www.instagram.com/dwkim_and",
+    email: "mailto:andandend2847@gmail.com",
+  },
+  utterances: {
+    repo: "whatisand/blog",
+    type: "pathname",
+  },
+}
