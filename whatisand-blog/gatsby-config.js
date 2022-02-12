33428e2fe13efb0bfaf93e1a991f88b34023a34a
Kim Dongwoo <andandend2847@gmail.com>
2022-02-13 02:20:16 +0900

    setting: 기본 url 수정 설정 적용

--- whatisand-blog/gatsby-config.js
+++ whatisand-blog/gatsby-config.js
@@ -2,7 +2,7 @@ const blogConfig = require("./blog-config")
 const { title, description, author, siteUrl } = blogConfig
 
 module.exports = {
-  pathPrefix: "/blog",
+  pathPrefix: "/",
   siteMetadata: {
     title,
     description,
